902bd35dc260653e9865014ee4faaf16a7436182
diegofly91 <diego_fernandolibreros@hotmail.com>
2022-09-08 20:22:42 -0500

    edit changes fields values from scalar

--- src/modules/auth/dtos/login-user.dto.ts
+++ src/modules/auth/dtos/login-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class LoginSiteDto {
     @MinLength(5, { message: 'el client_id es muy corto' })
@@ -6,9 +6,8 @@ export class LoginSiteDto {
     @IsNotEmpty()
     clientId: string;
 
-    @MinLength(2, { message: 'the name the site small' })
     @MaxLength(120, { message: 'the name site is long caracteres' })
     @IsString()
-    @IsNotEmpty()
+    @IsOptional()
     site: string;
 }
